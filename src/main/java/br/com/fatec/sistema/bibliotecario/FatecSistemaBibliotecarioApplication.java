50a2edbef120b34833c2e7486ba37fe0826f1084
paulosedassari <pssantos.ph200@gmail.com>
2022-09-26 17:42:11 -0300

    Primeiro commit da aplicação.

---
+++ src/main/java/br/com/fatec/sistema/bibliotecario/FatecSistemaBibliotecarioApplication.java
@@ -0,0 +1,13 @@
+package br.com.fatec.sistema.bibliotecario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FatecSistemaBibliotecarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FatecSistemaBibliotecarioApplication.class, args);
+	}
+
+}
