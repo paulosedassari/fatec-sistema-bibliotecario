8ef73fbc38763d2c5cd392bc686b7933c5fcd700
paulosedassari <pssantos.ph200@gmail.com>
2022-10-14 22:38:53 -0300

    Ajuste controller usuário.

--- src/main/java/br/com/fatec/sistema/bibliotecario/model/Endereco.java
+++ src/main/java/br/com/fatec/sistema/bibliotecario/model/Endereco.java
@@ -1,5 +1,7 @@
 package br.com.fatec.sistema.bibliotecario.model;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -11,33 +13,39 @@ import lombok.Data;
 
 @Data
 @Entity
-public class Endereco {
+public class Endereco implements Serializable {
+
+	private static final long serialVersionUID = 2561274389411758845L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idEndereco = 0l;
 
+	private Integer cep = 0;
 	private String estado = "";
-	private String cidade = "";
+	private String localidade = "";
 	private String bairro = "";
-	private String rua = "";
+	private String logradouro = "";
 	private Integer numero = 0;
-	
+	private String uf = "";
+
 	@ManyToOne
 	@JoinColumn(name = "usuairo_id")
 	private Usuario usuario;
 
 	public Endereco() {
-		
+
 	}
 
-	public Endereco(String estado, String cidade, String bairro, String rua, Integer numero,
-			Usuario usuario) {
+	public Endereco(Integer cep, String estado, String localidade, String bairro, String logradouro,
+			Integer numero, String uf, Usuario usuario) {
+		this.cep = cep;
 		this.estado = estado;
-		this.cidade = cidade;
+		this.localidade = localidade;
 		this.bairro = bairro;
-		this.rua = rua;
+		this.logradouro = logradouro;
 		this.numero = numero;
+		this.uf = uf;
 		this.usuario = usuario;
 	}
 
--- src/main/java/br/com/fatec/sistema/bibliotecario/model/Usuario.java
+++ src/main/java/br/com/fatec/sistema/bibliotecario/model/Usuario.java
@@ -1,5 +1,6 @@
 package br.com.fatec.sistema.bibliotecario.model;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -17,32 +18,60 @@ import lombok.Data;
 
 @Data
 @Entity
-public class Usuario {
+public class Usuario implements Serializable {
+
+	private static final long serialVersionUID = -3610385707435226050L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long idUsuario = 0l;
 
 	private String nome = "";
-
 	private String cpf = "";
-
+	private String email = "";
+	private String telefone = "";
 	private LocalDate dtNascimento;
-
 	private LocalDateTime dtInclusao = LocalDateTime.now();
-
 	private Long ra = 0l;
+	private Integer cep = 0;
+	private String localidade = "";
+	private String bairro = "";
+	private String logradouro = "";
+	private Integer numero = 0;
+	private String uf = "";
 
 	@Enumerated(EnumType.STRING)
-	private Status condicao;
+	private Status categoria;
 
-	@OneToMany(mappedBy = "usuario")
-	private List<Endereco> endereco;
-
-	@OneToMany(mappedBy = "usuario")
-	private List<Telefone> telefone;
+//	@OneToMany(mappedBy = "usuario")
+//	private List<Telefone> telefone;
 
 	@OneToMany(mappedBy = "usuario")
 	private List<Acervo> obras;
+
+	public Usuario() {
+
+	}
+
+	public Usuario(Long idUsuario, String nome, String cpf, String email, String telefone, LocalDate dtNascimento,
+			LocalDateTime dtInclusao, Long ra, Integer cep, String localidade, String bairro, String logradouro,
+			Integer numero, String uf, Status categoria, List<Acervo> obras) {
+		this.idUsuario = idUsuario;
+		this.nome = nome;
+		this.cpf = cpf;
+		this.email = email;
+		this.telefone = telefone;
+		this.dtNascimento = dtNascimento;
+		this.dtInclusao = dtInclusao;
+		this.ra = ra;
+		this.cep = cep;
+		this.localidade = localidade;
+		this.bairro = bairro;
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.uf = uf;
+		this.categoria = categoria;
+		this.obras = obras;
+	}
 
 }
